aaf2f67043318089e7b4e61ee33a6602adf787c2
Ravêl Samal <63311371+ravelsultan@users.noreply.github.com>
2023-04-04 19:24:14 +0300

    Initial commit

---
+++ frontend/tailwind.config.js
@@ -0,0 +1,12 @@
+/** @type {import('tailwindcss').Config} */
+export default {
+  future: {
+    removeDeprecatedGapUtilities: true,
+  },
+  content: ['./src/**/*.{html,js,svelte,ts}'],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+}
+
